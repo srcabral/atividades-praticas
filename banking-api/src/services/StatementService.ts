b476cdbe8216bd4080223ca69c2287ce3f3ebd93
Sergio Cabral <scastrocabral@gmail.com>
2024-08-31 09:24:33 -0300

    add: método para imprimir todos os extratos

--- banking-api/src/services/StatementService.ts
+++ banking-api/src/services/StatementService.ts
@@ -23,7 +23,34 @@ class StatementService {
             throw error
         }
     }
-    
+
+    async withdraw(idCheckingAccount: string, amount: number, description: string) {
+        try {
+            if (amount <= 0) {
+                throw new Error("Invalid amount!")
+            }
+
+            const balance = await this.getBalance(idCheckingAccount)
+
+            if (amount > balance) {
+                throw new Error("Insufficient funds.")
+            }
+
+            const statement = await prisma.statement.create({
+                data: {
+                    idCheckingAccount,
+                    amount: amount * -1,
+                    description,
+                    type: "debit"
+                }
+            })
+
+            return statement
+        } catch (error) {
+            console.error(`Error creating withdraw: ${error}`)
+            throw error
+        }
+    }
 
     async getBalance(idCheckingAccount: string) {
         try {
@@ -38,6 +65,23 @@ class StatementService {
             return aggregate._sum.amount ?? 0
         } catch (error) {
             console.error(`Error fetching balance. ${error}`)
+            throw error
+        }
+    }
+
+    async getAll(idCheckingAccount: string) {
+        try {
+            const statement = await prisma.statement.findMany({
+                where: {
+                    idCheckingAccount
+                },
+                orderBy: {
+                    createdAt: "desc"
+                }
+            })
+            return statement
+        } catch (error) {
+            console.error(`Error fetching statement. ${error}`)
             throw error
         }
     }
